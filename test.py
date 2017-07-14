57755016634f2b4bc692f0b93383114381084dc4
Kapica,  Norbert <norbert.kapica@mobica.com>
2017-07-14 13:42:25 +0200

    Veryfing add categories, add tasks and logout implemented

--- test.py
+++ test.py
@@ -37,14 +37,41 @@ def check_login(driver, test_string):
     usubmit.click()
 
 
-def check_add_category():
+def check_add_category(driver, test_string):
     """Verify proper working of adding categories"""
-    pass
+    sidebar = driver.find_element_by_css_selector("span[class*="
+                                                  "'glyphicon-align']")
+
+    # if sidebar.parent._web_element_cls.get_attribute(class) ==
+    # ("sidebar-toggle sidebar-toggle-opened"):
+
+    cname = driver.find_element_by_css_selector(
+        "form[action='/add-category/'] > span > input[name='category']")
+    csubmit = driver.find_element_by_css_selector(
+        "form[action='/add-category/'] > span > input[type='submit']")
+
+    sidebar.click()
+    cname.send_keys(test_string)
+    csubmit.click()
 
 
-def check_add_task():
+def check_add_task(driver):
     """Verify proper working of adding tasks"""
-    pass
+    add_btn = driver.find_element_by_css_selector("button[class='floating-action-icon-add']")
+
+    add_title = driver.find_element_by_css_selector(
+        "form[action='/add/'] input[name='title']")
+    add_priority = driver.find_elements_by_css_selector(
+        "form[action='/add/'] input[name='priority']")
+    add_category = driver.find_elements_by_css_selector(
+        "form[action='/add/'] select[name='category']")
+    add_submit = driver.find_elements_by_css_selector("input[id='addNoteBtn']")
+
+    add_btn.click()
+    add_title.send_keys("testtask")
+    add_priority[1].click()
+    add_category.value_of_css_property()
+    add_submit.click()
 
 
 def check_edit_task():
@@ -77,9 +104,11 @@ def checkif_todo():
     pass
 
 
-def check_logout():
+def check_logout(driver):
     """Verify proper working of logout task"""
-    pass
+    logout_btn = driver.find_element_by_css_selector(
+        "button[data-original-title='Logout']")
+    logout_btn.click()
 
 
 def main():
@@ -88,8 +117,13 @@ def main():
     driver.get("http://localhost:8081")
 
     test_string = "bambo"
-    check_register(driver, test_string)
+    # check_register(driver, test_string)
     check_login(driver, test_string)
+
+    # check_add_category(driver, "cat1")
+    # check_add_category(driver, "cat2")
+    # check_add_task(driver)
+    check_logout(driver)
 
     raw_input("Press enter key to exit\n")
     driver.quit()
