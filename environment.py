7d0a7d0e7130e246c1d1640ac5fa2f17bbb4c931
Kapica,  Norbert <norbert.kapica@mobica.com>
2017-07-21 15:07:47 +0200

    Dynamic containerization for every testing

--- environment.py
+++ environment.py
@@ -8,18 +8,18 @@ def before_all(context):
     # CLIENT = docker.APIClient()
     # APP_IMAGE = CLIENT.build(path=".", tag="selenium_tutorial:latest", rm=True)
 
-    # context.client = docker.from_env()
-    # context.app_container = context.client.containers.run(
-    #     "selenium_tutorial:latest", ports={'8081/tcp': 8081}, detach=True,
-    #     name='selenium_app',
-    #     volumes={
-    #         '/home/int_noka/Desktop/python/selenium_tutorial/App':
-    #         {'bind': '/go/src/github.com/thewhitetulip/Tasks', 'mode': 'rw'}})
+    context.client = docker.from_env()
+    context.app_container = context.client.containers.run(
+        "selenium_tutorial:latest", ports={'8081/tcp': 8081}, detach=True,
+        name='selenium_app',
+        volumes={
+            '/home/int_noka/Desktop/python/selenium_tutorial/App':
+            {'bind': '/go/src/github.com/thewhitetulip/Tasks', 'mode': 'rw'}})
     context.driver = webdriver.Chrome()
 
 
 def after_all(context):
     """Post-test behaviors"""
     context.driver.close()
-    # context.app_container.stop()
-    # context.app_container.remove()
+    context.app_container.stop()
+    context.app_container.remove()
